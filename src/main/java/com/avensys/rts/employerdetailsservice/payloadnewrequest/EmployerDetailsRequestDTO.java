a76198b61888fd4309fa13db2f309abecdc6a1f7
rku-avensys <rahul@aven-sys.com>
2024-02-23 16:59:24 +0530

    removed @data annotation

--- src/main/java/com/avensys/rts/employerdetailsservice/payloadnewrequest/EmployerDetailsRequestDTO.java
+++ src/main/java/com/avensys/rts/employerdetailsservice/payloadnewrequest/EmployerDetailsRequestDTO.java
@@ -1,10 +1,12 @@
 package com.avensys.rts.employerdetailsservice.payloadnewrequest;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Setter
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class EmployerDetailsRequestDTO {
